4fe626b20fcca886a532e89bd8411e8697c81849
agarwalmanvi <94.manviagarwal@gmail.com>
2017-09-29 23:51:15 +0530

    Added knowledge operators, public announcements

--- modal.py
+++ modal.py
@@ -15,6 +15,7 @@ class Formula:
 		return Diamond(self)
 		
 class Atom(Formula):
+	op = ""
 	#Calling as: 
 	# a = Atom('a')
 	def __init__(self, name):
@@ -24,8 +25,8 @@ class Atom(Formula):
 	def __hash__(self):
 		return hash(self.name)
 	#Call a.v(prop) : reduces truth to membership
-	def v(self, prop):
-		return self in prop
+	def v(self, model, world):
+		return self in model.nodes[world]
 	def __str__(self):
 		return str(self.name)
 	__repr__ = __str__
@@ -44,23 +45,23 @@ class BinOp(Formula):
 	
 class And(BinOp):
 	op = '&'
-	def v(self, prop):
-		return self.lchild.v(prop) and self.rchild.v(prop)
+	def v(self, model, world):
+		return self.lchild.v(model, world) and self.rchild.v(model, world)
 	
 class Or(BinOp):
 	op = '|'
-	def v(self, prop):
-		return self.lchild.v(prop) or self.rchild.v(prop)
+	def v(self, model, world):
+		return self.lchild.v(model, world) or self.rchild.v(model, world)
 
 class Imp(BinOp):
 	op = '->'
-	def v(self, prop):
-		return not self.lchild.v(prop) or self.rchild.v(prop)
+	def v(self, model, world):
+		return not self.lchild.v(model, world) or self.rchild.v(model, world)
 		
 class Iff(BinOp):
 	op = '<->'
-	def v(self, prop):
-		return self.lchild.v(prop) is self.rchild.v(prop)
+	def v(self, model, world):
+		return self.lchild.v(model, world) is self.rchild.v(model, world)
 		
 class UnOp(Formula):
 	def __init__(self, child):
@@ -70,27 +71,78 @@ class UnOp(Formula):
 		
 class Not(UnOp):
 	op = '~'
-	def v(self, prop):
-		return not self.child.v(prop)
-		
+	def v(self, model, world):
+		return not self.child.v(model, world)
+
 class Box(UnOp):
 	op = '[]'
 	def v(self, model, world):
 		nodes = []
-		for i in range(len(model.edges)):
-			if model.edges[i][0] == world:
-				nodes.append(model.edges[i][1])
-		if not nodes:
-			return True
-		else:
-			return all(self.child.v(model.nodes[i]) for i in nodes)
-			
+		for i in range(len(model.nonAgentEdges)):
+			if model.nonAgentEdges[i][0] == world:
+				nodes.append(model.nonAgentEdges[i][1])
+		#print "All worlds reachable from", world,": ", nodes
+		return all(self.child.v(model, k) for k in nodes)
+		
 class Diamond(UnOp):
 	op = '<>'
 	def v(self, model, world):
 		nodes = []
-		for i in range(len(model.edges)):
-			if model.edges[i][0] == world:
-				nodes.append(model.edges[i][1])
-		return any(self.child.v(model.nodes[i]) for i in nodes)
+		for i in range(len(model.nonAgentEdges)):
+			if model.nonAgentEdges[i][0] == world:
+				nodes.append(model.nonAgentEdges[i][1])
+		#print "All worlds reachable from", world,": ", nodes
+		return any(self.child.v(model, k) for k in nodes)
 
+class EpistemicOp(Formula):
+	def __init__(self, agent, child):
+		self.agent = agent
+		self.child = child
+	def __str__(self):
+		return '(' + self.op + '_' + str(self.agent) + str(self.child) + ')'
+
+class K(EpistemicOp):
+	op = 'K'
+	def v(self, model, world):
+		nodes = []
+		for i in range(len(model.edges[self.agent])):
+			if model.edges[self.agent][i][0] == world:
+				nodes.append(model.edges[self.agent][i][1])
+		print "All worlds reachable from", world," by agent", self.agent, ": ", nodes
+		return all(self.child.v(model, k) for k in nodes)
+		
+class M(EpistemicOp):
+	op = 'M'
+	def v(self, model, world):
+		nodes = []
+		for i in range(len(model.edges[self.agent])):
+			if model.edges[self.agent][i][0] == world:
+				nodes.append(model.edges[self.agent][i][1])
+		print "All worlds reachable from", world," by agent", self.agent, ": ", nodes
+		return any(self.child.v(model, k) for k in nodes)
+		
+class AnnounceOp(Formula):
+	def __init__(self, formula, child):
+		self.formula = formula
+		self.child = child
+	def __str__(self):
+		return '(' + '(' + self.op + '_' + str(self.formula) + ')' + str(self.child) + ')'
+		
+class Announce(AnnounceOp):
+	op = '[[]]'
+	def v(self, model, world):
+		nodesToDelete = []
+		for key in model.nodes.keys():
+			if self.formula.v(model, key) == False:
+				nodesToDelete.append(key)
+		for i in nodesToDelete:
+			model.delNode(i)
+		return self.child.v(model, world)
+			
+			
+			
+			
+			
+			
+			
+			
--- model.py
+++ model.py
@@ -1,15 +1,31 @@
-from mylogic import *
+from modal import *
+from copy import deepcopy
 
 class Model:
 	def __init__(self):
-		#dict recording which propositional atoms are true in which worlds
 		self.nodes = dict()
-		self.edges = []
+		self.edges = dict()
+		self.nonAgentEdges = []
 		self.worldsNum = 0
 		self.agents = []
+		self.type = 'S5' #Can be changed to 'K': this simply influences how you add edges, S5 adds edges wrt ref, sy, trans/K adds 						edges individually
+	
+	def addAgent(self, agent):
+		if agent not in self.agents:
+			self.agents.append(agent)
+			self.edges[agent] = []
+			if self.nodes:
+				for key in self.nodes.keys():
+					self.edges[agent].append((key, key))
+		else:
+			print "That agent already exists!"
 	
 	def addNode(self):
 		self.nodes[self.worldsNum] = []
+		#When you create a world you it is automatically reflexive for all agents if type is S5
+		if self.type == 'S5':
+			for i in self.agents:
+				self.edges[agent].append((self.worldsNum, self.worldsNum))
 		self.worldsNum = self.worldsNum + 1
 		
 	def addNodes(self, num):
@@ -24,15 +40,98 @@ class Model:
 			self.nodes[i].append(prop)
 			
 	#add edges individually
-	def addEdge(self, w1, w2):
-		if (w1,w2) not in self.edges:
-			if w1<self.worldsNum and w2<self.worldsNum:
-				self.edges.append((w1,w2))
+	def addEdge(self, w1, w2, agent):
+		if self.type == 'K':
+			if (w1,w2) not in self.edges[agent]:
+				if w1<self.worldsNum and w2<self.worldsNum and agent in self.agents:
+					self.edges[agent].append((w1,w2))
+					if (w1, w2) not in self.nonAgentEdges:
+						self.nonAgentEdges.append((w1,w2))
+				else:
+					print "Sorry; those agent/world(s) don't exist!"
 			else:
-				print "Sorry; those world(s) don't exist!"
+				print "This edge already exists."
 		else:
-			print "This edge already exists."
-			
-	#For propositional formulas
-	def evaluate(self, formula, node):
-		return formula.v(self.nodes[node])
+			if (w1,w2) not in self.edges[agent]:
+				if w1<self.worldsNum and w2<self.worldsNum and agent in self.agents:
+					self.edges[agent].append((w1,w2))			#
+					if (w1, w2) not in self.nonAgentEdges:			#
+						self.nonAgentEdges.append((w1,w2))		#		
+					self.edges[agent].append((w2,w1))			# SYMMETRIC
+					if (w2, w1) not in self.nonAgentEdges:			#
+						self.nonAgentEdges.append((w2,w1))		#	
+					for key in self.nodes.keys():
+						if (key,w1) in self.edges[agent] and (w1,w2) in self.edges[agent]:
+							if (key,w2) not in self.edges[agent]:			#
+								self.edges[agent].append((key,w2))		#
+							if (key, w2) not in self.nonAgentEdges:			#
+								self.nonAgentEdges.append((key,w2))		# TRANSITIVE
+							if (w2,key) not in self.edges[agent]:			#	
+								self.edges[agent].append((w2,key))		#
+							if (w2, key) not in self.nonAgentEdges:			#
+								self.nonAgentEdges.append((w2,key))
+						if (w2,w1) in self.edges[agent] and (w1,key) in self.edges[agent]:
+							if (w2,key) not in self.edges[agent]:
+								self.edges[agent].append((w2,key))
+							if (w2, key) not in self.nonAgentEdges:
+								self.nonAgentEdges.append((w2,key))
+							if (key,w2) not in self.edges[agent]:
+								self.edges[agent].append((key,w2))
+							if (key, w2) not in self.nonAgentEdges:
+								self.nonAgentEdges.append((key,w2))
+				else:
+					print "Sorry; those agent/world(s) doesn't/don't exist!"
+			else:
+				print "This edge already exists."
+
+	def delNode(self, node):
+		if node in self.nodes:
+			del self.nodes[node]
+			copyEdges = dict()
+			for i in self.agents:
+				copyEdges[i] = []
+			for i in self.agents:
+				for j in self.edges[i]:
+					if j[0] != node and j[1] != node:
+						copyEdges[i].append(j)
+			self.edges = deepcopy(copyEdges)
+			copyNonAgentEdges = []
+			for i in self.nonAgentEdges:
+				if i[0] != node and i[1] != node:
+					copyNonAgentEdges.append(i)
+			self.nonAgentEdges = deepcopy(copyNonAgentEdges)
+		else:
+			print "That node doesn't exist!"
+					
+
+
+
+
+
+
+
+
+
+"""		
+	def draw(self):
+		g = Graph()
+		valuation = g.new_vertex_property("vector<int>")
+		for i in range(self.worldsNum):
+			g.add_vertex()
+			valuation(g.vertex(i)) = m.nodes[i]
+"""
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
